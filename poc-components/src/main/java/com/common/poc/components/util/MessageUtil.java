eb667af04d0ca352923b2cdeea4d153ae9f95604
Nick <huangbiyun1512@163.com>
2020-09-15 17:31:35 +0800

    feature: utils

---
+++ poc-components/src/main/java/com/common/poc/components/util/MessageUtil.java
@@ -0,0 +1,73 @@
+package com.common.poc.components.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+@Slf4j
+public class MessageUtil {
+
+  private final MessageSource messageSource;
+
+  public MessageUtil(
+      @Autowired MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
+
+  public String get(String key) {
+    return this.get(key, null, LocaleContextHolder.getLocale());
+  }
+
+  public String get(String key, Object[] objects) {
+    return this.get(key, objects, LocaleContextHolder.getLocale());
+  }
+
+  public String get(String key, Object[] objects, Locale locale) {
+    return messageSource.getMessage(key, objects, locale);
+  }
+
+  public String getCode(String key) {
+    return this.get(
+        StringUtils.join(key, MESSAGE_SUFFIX_CODE),
+        null,
+        LocaleContextHolder.getLocale());
+  }
+
+  public String getTitle(String key) {
+    return this.get(
+        StringUtils.join(key, MESSAGE_SUFFIX_TITLE),
+        null,
+        LocaleContextHolder.getLocale());
+  }
+
+  public String getTitle(String key, Object[] objects) {
+    return this.get(
+        StringUtils.join(key, MESSAGE_SUFFIX_TITLE),
+        objects,
+        LocaleContextHolder.getLocale());
+  }
+
+  public String getDetail(String key) {
+    return this.get(
+        StringUtils.join(key, MESSAGE_SUFFIX_DETAIL),
+        null,
+        LocaleContextHolder.getLocale());
+  }
+
+  public String getDetail(String key, Object[] objects) {
+    return this.get(
+        StringUtils.join(key, MESSAGE_SUFFIX_DETAIL),
+        objects,
+        LocaleContextHolder.getLocale());
+  }
+
+  public static final String MESSAGE_SUFFIX_CODE = ".code";
+  public static final String MESSAGE_SUFFIX_TITLE = ".title";
+  public static final String MESSAGE_SUFFIX_DETAIL = ".detail";
+}
