b0c73a413387fc16926a327a7e086a155090cc3e
Nick <huangbiyun1512@163.com>
2020-10-22 10:05:12 +0800

    feature: BusinessException

--- apawnd-commons-component/src/main/java/com/maersk/apawnd/commons/component/exception/BusinessException.java
+++ apawnd-commons-component/src/main/java/com/maersk/apawnd/commons/component/exception/BusinessException.java
@@ -10,12 +10,28 @@ public class BusinessException extends RuntimeException {
 
   private final List<BaseErrorDto> errors;
 
+
   public BusinessException(List<BaseErrorDto> errors) {
+    this.errors = errors;
+  }
+
+  public BusinessException(String message) {
+    super(message);
+    this.errors = null;
+  }
+
+  public BusinessException(List<BaseErrorDto> errors, String message) {
+    super(message);
     this.errors = errors;
   }
 
   public BusinessException(List<BaseErrorDto> errors, Throwable cause) {
     super(cause);
+    this.errors = errors;
+  }
+
+  public BusinessException(List<BaseErrorDto> errors, String message, Throwable cause) {
+    super(message, cause);
     this.errors = errors;
   }
 }
